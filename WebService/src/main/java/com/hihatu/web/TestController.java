99142b97a6b4096581c48047e6410e008552257f
yswz23 <yswz23@gmail.com>
2016-11-10 11:51:59 +0900

    add log example

--- WebService/src/main/java/com/hihatu/web/TestController.java
+++ WebService/src/main/java/com/hihatu/web/TestController.java
@@ -1,5 +1,7 @@
 package com.hihatu.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +14,20 @@ import com.hihatu.service.TestService;
 @RequestMapping("/test")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private TestService testService;
 
     @RequestMapping("/select")
     public TestBean select(@RequestParam(value = "id", defaultValue = "0") Integer id) {
+        logger.debug("select");
         return testService.getTestBean(id);
     }
 
     @RequestMapping("/update")
     public String update(@RequestParam(value = "id") Integer id, @RequestParam(value = "content") String content) {
+        logger.info("update");
         if (id == null || content == null) {
             return "failed";
         }
